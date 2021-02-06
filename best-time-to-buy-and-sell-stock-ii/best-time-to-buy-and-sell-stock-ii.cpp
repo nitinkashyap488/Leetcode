37aa38d85a7abb2e4fe817e8e2ae850afe05b721
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-02-07 03:47:28 +0530

    Time: 0 ms (100.00%), Space: 13 MB (90.08%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=0;
+        if(prices.size()==1){
+            return 0;
+        }
+        for(int i=0;i<prices.size()-1;i++){
+            if(prices[i]<prices[i+1]){
+                ans+=prices[i+1]-prices[i];
+            }
+        }
+        return ans;
+    }
+};
