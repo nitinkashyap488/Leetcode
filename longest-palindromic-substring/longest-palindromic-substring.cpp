1969c5f8dfacbfb2c62f46f970b5d647a4a2ecb4
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-03-12 14:12:46 +0530

    Update longest-palindromic-substring.cpp

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -2,30 +2,22 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n= s.size();
-        if(n==0)
-        {
+        if(n==0){
             return "";
         }
         bool dp[n][n]; 
         memset(dp,0,sizeof(dp)); 
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             dp[i][i]=true;
         }
         string str ="";
         str+=s[0];
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(s[i]==s[j])
-                {
-                    if(j-i == 1 || dp[i+1][j-1])
-                    {
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(s[i]==s[j]){
+                    if(j-i == 1 || dp[i+1][j-1]){
                         dp[i][j] = true;
-                        
-                        if(str.size() < (j - i + 1))
-                        {
+                        if(str.size() < (j - i + 1)){
                             str = s.substr(i,j-i+1);
                         }
                     }
