b4ab312e2c25c5f0c6f01a38a125d6d9efc4a5cc
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-04-24 23:07:27 +0530

    Update search-a-2d-matrix.cpp

--- search-a-2d-matrix/search-a-2d-matrix.cpp
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,20 +3,22 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int i, start =0, end = m-1;
+        int start =0, end = n*m-1;
+        
         if(target<matrix[0][0] || target>matrix[n-1][m-1]){
             return false;
         }
-        for(i=0;i<n;i++){
-            if(target <= matrix[i][end]){
-                break;
-            }
-        }
+        
+        /*
+            matrix[mid/m][mid%m] gives coresponding row and column;
+        
+        */
+        
         while(start<=end){
             int mid = start +(end-start)/2;
-            if(matrix[i][mid]==target){
+            if( matrix[mid/m][mid%m] == target){  
                 return true;
-            }else if(matrix[i][mid]>target){
+            }else if( matrix[mid/m][mid%m] > target){
                 end = mid-1;
             }else{
                 start = mid+1;
