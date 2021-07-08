8399faf567c1a7447a51887e4c97bab44acad4ed
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-07-08 15:25:06 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (16.00%) - LeetHub

---
+++ ugly-number/ugly-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        int num = n;
+        while(n>0){
+            if(n%2==0){
+                n=n/2;
+            }else if(n%3==0){
+                n=n/3;
+            }else if(n%5==0){
+                n=n/5;
+            }else{
+                break;
+            }
+        }
+        if(n==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
