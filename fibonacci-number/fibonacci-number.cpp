e054ec3cbdec873412bc1eba8efe51b32e7390b2
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-07-13 00:17:50 +0530

    Update fibonacci-number.cpp

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0){
-            return 0;
+        if(n==0 || n==1){
+            return n;
         }
-        if(n==1){
-            return 1;
+        int a = 0;
+        int b = 1;
+        int c;
+        for(int i=2;i<=n;i++){
+            c = a + b;
+            a = b;
+            b = c;
         }
-        return fib(n-1)+fib(n-2);
+        return c;
     }
 };
+
+
+
