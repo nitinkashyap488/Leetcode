a3d183cfe236b2ae47b203feeabd1feb0c462c3f
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-07-13 00:26:30 +0530

    Time: 20 ms (8.31%), Space: 13 MB (90.68%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -4,9 +4,7 @@ public:
     /********************************** Kadane's Algorithm ****************************/
 
     int maxSubArray(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
-        }
+        
         int currSum = 0;
         int maxSum = INT_MIN;
         for(int i =0;i<nums.size();i++){
