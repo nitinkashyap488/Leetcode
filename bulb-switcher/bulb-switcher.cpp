fb63e40a7497ca5950e03d889aa2adbcec515f02
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-07-14 22:49:19 +0530

    Update bulb-switcher.cpp

--- bulb-switcher/bulb-switcher.cpp
+++ bulb-switcher/bulb-switcher.cpp
@@ -4,3 +4,14 @@ public:
         return sqrt(n);
     }
 };
+
+
+
+
+explanation
+lets us suppose there are 100 bulbs, all turned off in the beginning.
+in the first iteration, we turn on all the bulbs.
+in the second iteration, we start from 2 and turn off every 2nd bulb.... like 2,4,6...
+in the third iteration, we start from 3 and turn off every 3rd bulb.... like 3,6,9...
+
+as we follow the process we see a pattern that only the bulbs which are the square of any numbers are "ON" and the rest all bulbs are off.
