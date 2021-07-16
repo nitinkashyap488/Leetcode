57fffa882193c69a638c331bab09d9fba2afd181
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-07-16 13:40:39 +0530

    Update 2-keys-keyboard.cpp

--- 2-keys-keyboard/2-keys-keyboard.cpp
+++ 2-keys-keyboard/2-keys-keyboard.cpp
@@ -13,4 +13,16 @@ public:
         
         return dp.back();
     }
+    
+    
+    /****************************** Alternate Solution with faster than 100% submission ************/
+    // int ans = 0, d = 2;
+    //     while (n > 1) {
+    //         while (n % d == 0) {
+    //             ans += d;
+    //             n /= d;
+    //         }
+    //         d++;
+    //     }
+    //     return ans;
 };
