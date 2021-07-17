0b5a4d0613bd5eb48c60d74968c862e7c5bc3ec7
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-07-17 13:51:29 +0530

    Update count-numbers-with-unique-digits.cpp

--- count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
+        //9 * 9 + 10 for n = 2
+        //9 * 9 * 8 + 10 for n = 3
+        //9 * 9 * 8 * 7 + 10 for n = 4
+        //9 * 9 * 8 * 7 * 6 + 10 for n = 5
+        
         int ans = 10;
-        if(n==0){
+        if(n == 0){
             return 1;
-        }else if(n==1){
+        }else if(n == 1){
             return 10;
         }else{
-            int sum =9;
-            for(int i =2;i<=n;i++){
-                sum*=(11-i);
-                ans=ans+sum;
+            int product = 9;
+            for(int i = 2; i <= n; i++){
+                product = product * (11 - i);
+                ans += product;
             }
         }
         return ans;
