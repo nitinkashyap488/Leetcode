5c14f49c36d09a124fa60c9a8e0a8c70cb28de0f
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-07-17 17:24:12 +0530

    Update happy-number.cpp

--- happy-number/happy-number.cpp
+++ happy-number/happy-number.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    map<int,bool>dp;
     bool isHappy(int n) {
-        if(n==1){
-            return true;
-        }
-        if(dp[n]==true){
-            return false;
-        }
-        dp[n] = true;
         int sum =0;
         while(n>0){
-            int dig = n%10;
-            sum += dig*dig;
-            n = n/10;
+            sum+= pow(n%10,2);
+            n/=10;
         }
-        return isHappy(sum);
+        if(sum ==1 || sum ==7){
+            return true;
+        }else if(sum<10){
+            return false;
+        }else{
+            return isHappy(sum);
+        }
     }
 };
