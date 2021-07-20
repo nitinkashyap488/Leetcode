1e617eca9ebce40a4776a9de36f207900ed75a3f
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-07-21 01:57:21 +0530

    Update increasing-triplet-subsequence.cpp

--- increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int firstMin=INT_MAX;
-        int secondMin=INT_MAX;
+        int low = INT_MAX;
+        int mid = INT_MAX;
         
         for(int i=0;i<nums.size();i++){
             
-            if(nums[i] <= firstMin){
-                firstMin = nums[i];
-            }else if(nums[i] <= secondMin){
-                secondMin = nums[i];
+            if(nums[i] <= low){
+                low = nums[i];
+            }else if(nums[i] <= mid){
+                mid = nums[i];
             }else{
                 return true;
             }
