afba9b99ebba6c801974b02a51ca483feaee592d
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2021-07-21 00:47:49 +0530

    Update factorial-trailing-zeroes.cpp

--- factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int count=0,temp=n;
-        while(temp){
-            temp=temp/5;
-            count++;
+        int count = 0;
+        int x = 5;
+        while(x<=n){
+            count+= n/x;
+            x = x*5;
         }
-        int ans=0;
-        for(int i=1;i<=count;i++){
-            ans+= n/pow(5,i);
-        }
-        return ans;
+        return count;
+        
+        
+// ************************************* OLD Solution *******************************************//
+        // int count=0,temp=n;
+        // while(temp){
+        //     temp=temp/5;
+        //     count++;
+        // }
+        // int ans=0;
+        // for(int i=1;i<=count;i++){
+        //     ans+= n/pow(5,i);
+        // }
+        // return ans;
     }
 };
