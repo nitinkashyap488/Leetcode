e5ed48a630140f57b4807a8af9c74f6b531f805c
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2022-01-02 23:46:18 +0530

    Update valid-parentheses.cpp

--- valid-parentheses/valid-parentheses.cpp
+++ valid-parentheses/valid-parentheses.cpp
@@ -1,27 +1,53 @@
+// New solution
+
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(!stk.empty()){
-                if(s[i]=='('||s[i]=='{'||s[i]=='['){
-                    stk.push(s[i]);
-                }else{
-                    if(s[i]==')' && stk.top()=='('){
-                        stk.pop();
-                    }else if(s[i]=='}' && stk.top()=='{'){
-                        stk.pop();
-                    }else if(s[i]==']' && stk.top()=='['){
-                        stk.pop();
-                    }else{
-                        stk.push(s[i]);
-                    }
-                }
+        for(auto x: s){
+            if(x == '(' or x == '{' or x == '['){
+                stk.push(x);
+            }else if(!stk.empty() and x == ')' and stk.top() == '('){
+                stk.pop();
+            }else if(!stk.empty() and x == '}' and stk.top() == '{'){
+                stk.pop();
+            }else if(!stk.empty() and x == ']' and stk.top() == '['){
+                 stk.pop();
             }else{
-                stk.push(s[i]);
-            }            
+                stk.push(x);
+            }
         }
         return stk.empty();
     }
 };
+
+
+// Old Solution :
+
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> stk;
+//         int n=s.size();
+//         for(int i=0;i<n;i++){
+//             if(!stk.empty()){
+//                 if(s[i]=='('||s[i]=='{'||s[i]=='['){
+//                     stk.push(s[i]);
+//                 }else{
+//                     if(s[i]==')' && stk.top()=='('){
+//                         stk.pop();
+//                     }else if(s[i]=='}' && stk.top()=='{'){
+//                         stk.pop();
+//                     }else if(s[i]==']' && stk.top()=='['){
+//                         stk.pop();
+//                     }else{
+//                         stk.push(s[i]);
+//                     }
+//                 }
+//             }else{
+//                 stk.push(s[i]);
+//             }            
+//         }
+//         return stk.empty();
+//     }
+// };
