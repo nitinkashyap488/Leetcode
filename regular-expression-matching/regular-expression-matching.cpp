ab77299ae77f07e830b70625a2f4845f425687c8
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2022-01-02 22:41:21 +0530

    Time: 245 ms (5.01%), Space: 13.6 MB (17.20%) - LeetHub

--- regular-expression-matching/regular-expression-matching.cpp
+++ regular-expression-matching/regular-expression-matching.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        return regex_match(s,regex(p));
+        if(p.empty()){
+            return s.empty();
+        }
+        
+        bool match = s.size() > 0 and ((s[0] == p[0]) or (p[0] == '.'));
+        if(p.size() > 1 && p[1] == '*'){
+            return isMatch(s,p.substr(2)) or (match and isMatch(s.substr(1),p));
+        }else{
+            return match and isMatch(s.substr(1),p.substr(1));
+        }
     }
 };
+
+
+// one Liner Solution 
+
+// class Solution {
+// public:
+//     bool isMatch(string s, string p) {
+//         return regex_match(s,regex(p));
+//     }
+// };
