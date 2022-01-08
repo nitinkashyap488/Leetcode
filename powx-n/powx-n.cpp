f9be46b01b20a8772bcf499e477ec4d450c56b45
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2022-01-08 08:07:41 +0530

    Update powx-n.cpp

--- powx-n/powx-n.cpp
+++ powx-n/powx-n.cpp
@@ -15,3 +15,32 @@ public:
         return ans;
     }
 };
+
+
+
+// class Solution {
+//     double helper(double x, int n){
+//         if(n == 0){
+//             return 1;
+//         }
+//         if(n == 1){
+//             return x;
+//         }
+        
+//         double ans = helper(x, n/2);
+//         if(n%2 == 0){
+//             return ans*ans;
+//         }else{
+//             return x*ans*ans;
+//         }
+//     }
+// public:
+//     double myPow(double x, int n) {
+//         double ans = helper(x,n);
+//         if(n>=0){
+//             return ans;
+//         }else{
+//             return 1/ans;
+//         }
+//     }
+// };
