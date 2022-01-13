ff1d9fd97bf92c151ecaaf83efe303656c6cd80a
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2022-01-13 15:34:08 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (95.14%) - LeetHub

---
+++ word-break/word-break.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<bool> dp(n+1, false);
+        dp[n] = true;
+        
+        for(int i = n-1; i>=0; i--){
+            for(auto w:wordDict){
+                int t = w.size();
+                if((i+t <= n) and s.substr(i,t) == w){
+                    dp[i] = dp[i + t];
+                }
+                if(dp[i]==true)break;
+            }
+        }        
+        return dp[0];
+    }
+};
