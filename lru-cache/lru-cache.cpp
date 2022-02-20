2d52d544d6f9614528623f6c0adae250f5f7d5c3
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2022-02-20 23:49:02 +0530

    Time: 651 ms (44.02%), Space: 165.1 MB (68.89%) - LeetHub

--- lru-cache/lru-cache.cpp
+++ lru-cache/lru-cache.cpp
@@ -1,35 +1,40 @@
-class LRUCache
-{
-    public:
-        list<pair<int,int>> doubleList;
-        unordered_map<int,list<pair<int, int>>::iterator> mp;
-        int size;
-        LRUCache(int capacity)
-        {
-            size=capacity;
+class LRUCache {
+    list<pair<int,int>> doubleList;
+    unordered_map<int,list<pair<int, int>>::iterator> mp;
+    int size;
+public:
+    LRUCache(int capacity) {
+        size = capacity;
+    }
+    
+    int get(int key) {
+        if(mp.find(key) == mp.end()){
+            return -1;
         }
-        int get(int key)
-        {
-            if(mp.find(key)==mp.end())
-                return -1;
-            doubleList.splice(doubleList.begin(),doubleList,mp[key]);
-            return mp[key]->second;
+        doubleList.splice(doubleList.begin(), doubleList, mp[key] );
+        return mp[key] -> second;
+    } 
+    
+    void put(int key, int value) {
+        if(mp.find(key) != mp.end()){
+            doubleList.splice(doubleList.begin(), doubleList, mp[key]);
+            mp[key] -> second = value;
+            return;
         }
-        void put(int key, int value)
-        {
-            if(mp.find(key)!=mp.end())
-            {
-                doubleList.splice(doubleList.begin(),doubleList,mp[key]);
-                mp[key]->second=value;
-                return;
-            }
-            if(doubleList.size()==size)
-            {
-                auto temp = doubleList.back().first;
-                doubleList.pop_back();
-                mp.erase(temp);
-            }
-            doubleList.push_front({key,value});
-            mp[key]=doubleList.begin();
+        
+        if(doubleList.size() == size){
+            auto temp = doubleList.back().first;
+            doubleList.pop_back();
+            mp.erase(temp);
         }
+        doubleList.push_front({key, value});
+        mp[key] = doubleList.begin();
+    }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
