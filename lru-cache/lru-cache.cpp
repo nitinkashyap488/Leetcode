988e563ee6152b3f47d61bc3147d0a95345f9f2d
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2022-02-20 23:47:05 +0530

    Time: 675 ms (39.73%), Space: 165.1 MB (68.89%) - LeetHub

---
+++ lru-cache/lru-cache.cpp
@@ -0,0 +1,35 @@
+class LRUCache
+{
+    public:
+        list<pair<int,int>> doubleList;
+        unordered_map<int,list<pair<int, int>>::iterator> mp;
+        int size;
+        LRUCache(int capacity)
+        {
+            size=capacity;
+        }
+        int get(int key)
+        {
+            if(mp.find(key)==mp.end())
+                return -1;
+            doubleList.splice(doubleList.begin(),doubleList,mp[key]);
+            return mp[key]->second;
+        }
+        void put(int key, int value)
+        {
+            if(mp.find(key)!=mp.end())
+            {
+                doubleList.splice(doubleList.begin(),doubleList,mp[key]);
+                mp[key]->second=value;
+                return;
+            }
+            if(doubleList.size()==size)
+            {
+                auto temp = doubleList.back().first;
+                doubleList.pop_back();
+                mp.erase(temp);
+            }
+            doubleList.push_front({key,value});
+            mp[key]=doubleList.begin();
+        }
+};
