c9f8bfdbf004b365fe671416f89b64b487c78c16
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2024-03-17 21:58:58 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (78.92%) - LeetHub

--- search-insert-position/search-insert-position.java
+++ search-insert-position/search-insert-position.java
@@ -1,16 +1,16 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int i =0, j = nums.length -1;
+        int ans = nums.length;
         while(i <= j){
-            int mid = i + (j-i)/2;
-            if(target > nums[mid]){
-                i = mid + 1;
-            }else if ( target < nums[mid]){
+            int mid = i + (j - i)/2;
+            if(nums[mid] >= target){
+                ans = mid;
                 j = mid - 1;
-            }else {
-                return mid;
+            }else{
+                i = mid + 1;
             }
         }
-        return i ;
+        return ans;
     }
 }
