c8ccbb5e31d9caab9383d6077a0b159e3696ee16
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2024-03-26 22:28:01 +0530

    Update longest-common-prefix.java

--- longest-common-prefix/longest-common-prefix.java
+++ longest-common-prefix/longest-common-prefix.java
@@ -7,8 +7,7 @@ class Solution {
         String firstStr = strs[0];
         String lastStr = strs[strs.length - 1];
         StringBuilder sb = new StringBuilder();
-        int minLength = Math.min(firstStr.length(), lastStr.length());
-        for (int i = 0; i < minLength; i++) {
+        for (int i = 0; i < firstStr.length(); i++) {
             if (firstStr.charAt(i) == lastStr.charAt(i)) {
                 sb.append(firstStr.charAt(i));
             } else {
