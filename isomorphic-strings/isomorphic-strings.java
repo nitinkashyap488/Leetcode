c37b33c1adf533c0cbe5902ef36c548c2edfa8b3
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2024-03-27 11:48:44 +0530

    Update isomorphic-strings.java

--- isomorphic-strings/isomorphic-strings.java
+++ isomorphic-strings/isomorphic-strings.java
@@ -3,19 +3,34 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        Map<Character, Character> map = new HashMap<>();
-        for(int i = 0; i <s.length(); i++){
-            if(map.containsKey(s.charAt(i))){
-                if(map.get(s.charAt(i)) != t.charAt(i)){
-                    return false;
-                }
-            }else {
-                if(map.containsValue(t.charAt(i))){
-                    return false;
-                }   
-                map.put(s.charAt(i), t.charAt(i));
+        // Map<Character, Character> map = new HashMap<>();
+        // for(int i = 0; i <s.length(); i++){
+        //     if(map.containsKey(s.charAt(i))){
+        //         if(map.get(s.charAt(i)) != t.charAt(i)){
+        //             return false;
+        //         }
+        //     }else {
+        //         if(map.containsValue(t.charAt(i))){
+        //             return false;
+        //         }   
+        //         map.put(s.charAt(i), t.charAt(i));
+        //     }
+        // }
+        // return true;
+        
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) {
+                return false;
             }
+            
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
         }
+        
         return true;
     }
 }
