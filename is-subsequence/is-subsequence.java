636ea9656a02fa5e2b8f1b42825a12f64d504032
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2024-04-21 22:44:28 +0530

    Time: 2 ms (60.75%), Space: 41.2 MB (65.19%) - LeetHub

---
+++ is-subsequence/is-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.isEmpty()){
+            return true;
+        }
+        int i =0, j = 0;
+        while(i < s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+            if(i == s.length()){
+                return true;
+            }
+        }
+        return i == s.length();
+    }
+}
