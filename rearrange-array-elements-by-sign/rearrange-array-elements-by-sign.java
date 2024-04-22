b189afef317e5b48a81e17d6b615572ed5a6338a
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2024-04-22 16:51:21 +0530

    Time: 11 ms (9.29%), Space: 82.9 MB (17.50%) - LeetHub

--- rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,28 +1,30 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer> positive = new ArrayList<>();
-        List<Integer> negative = new ArrayList<>();
+        int n = nums.length;
+        int[] result = new int[n];
         
-        for(int x: nums){
-            if(x > 0){
-                positive.add(x);
-            }else{
-                negative.add(x);
+        List<Integer> positives = new ArrayList<>();
+        List<Integer> negatives = new ArrayList<>();
+        
+        for (int num : nums) {
+            if (num > 0) {
+                positives.add(num);
+            } else {
+                negatives.add(num);
             }
         }
         
-        int posIndex = 0;
-        int negIndex = 0;
-        for(int i =0; i< nums.length ; i++){
-            if(i % 2 == 0 && posIndex < positive.size()){
-                nums[i] = positive.get(posIndex++);
-            }else{
-                nums[i] = negative.get(negIndex++);
+        int positiveIndex = 0;
+        int negativeIndex = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                result[i] = positives.get(positiveIndex++);
+            } else {
+                result[i] = negatives.get(negativeIndex++);
             }
-            // if(i % 2 == 1 && negIndex < negative.size()){
-            //     nums[i] = negative.get(negIndex++);
-            // }
         }
-        return nums;
+        
+        return result;
     }
 }
