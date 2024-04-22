cd14389a4a85425a803d82c3dc9066454c6f4042
Nitin Shubham Kashyap <30380866+nitinkashyap488@users.noreply.github.com>
2024-04-22 12:39:24 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (81.06%) - LeetHub

--- find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,20 +1,13 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(needle.length() == 0){
+        if(haystack.equals(needle)){
             return 0;
         }
-        int  j = 0;
-        for(int i = 0; i < haystack.length(); i++){
-            if(haystack.charAt(i) == needle.charAt(j)){
-                j++;
-            }else{
-                i = i - j;
-                j = 0;
-            }
-            if(j == needle.length()){
-                return i - j + 1;
+        for(int i = 0; i < haystack.length() - needle.length() + 1 ; i++){
+            if((haystack.substring(i, i + needle.length())).equals(needle)){
+                return i;
             }
         }
-        return -1 ;
+        return -1;
     }
 }
